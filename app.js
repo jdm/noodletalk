7ac00d2a827031ca5ca6508f86d36494cbcfd923
Jen Fong-Adwent <jen@ednapiranha.com>
2012-03-06 20:13:10 -0500

    fixed session and redirect scope; also fixed spaces

--- app.js
+++ app.js
@@ -2,42 +2,53 @@ var https = require('https');
 var querystring = require('querystring');
 var routes = require('./routes');
 
-var app = require('./settings');
+var settings = require('./settings');
 
-// Routes
+var app = settings.app;
+var siteUrl = settings.options.domain;
 
+
+// Main landing page
 app.get('/', routes.index);
 
+// Browser ID login
 app.post('/login', function(req, res) {
-    var post_data = querystring.stringify({
-        'assertion': req.body.bid_assertion,
-        'audience' : 'http://localhost:3000',
+  var bid_response = '';
+  var post_data = querystring.stringify({
+    'assertion': req.body.bid_assertion,
+    'audience' : siteUrl,
+  });
+
+  var post_options = {
+    host: 'browserid.org',
+    path: '/verify',
+    port: 443,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Length': post_data.length
+    }
+  };
+
+  var post_req = https.request(post_options, function(resp) {
+    var body = '';
+    resp.setEncoding('utf-8');
+    resp.on('data', function(data) { body += data; });
+    resp.on('end', function() {
+      var bid_response = JSON.parse(body);
+      req.session.email = bid_response.email;
+      res.redirect('back');
     });
+  });
 
-    var post_options = {
-        host: 'browserid.org',
-        path: '/verify',
-        port: 443,
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Content-Length': post_data.length
-        }
-    };
+  post_req.write(post_data);
+  post_req.end();
+});
 
-    var post_req = https.request(post_options, function(res) {
-        res.setEncoding('utf-8');
-        res.on('data', function(data) {
-            var bid_response = JSON.parse(data);
-            req.session.email = bid_response.email;
-            console.log(req.session.email);
-        });
-    });
-
-    post_req.write(post_data);
-    post_req.end();
-
-    res.redirect('back');
+// Logout
+app.get('/logout', function(req, res) {
+  req.session.email = null;
+  res.redirect('back');
 });
 
 app.listen(3000);
